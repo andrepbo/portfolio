d2434104019926f01d5af115afcf32c07a9d5dee
Andre Oliveira <andre.pbo@gmail.com>
2025-01-23 19:17:16 +0000

    chore: update next config and github workflow

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "export", distDir: "out", images: { unoptimized: true } };
+const nextConfig = {
+    output: "export",
+    distDir: "out",
+    trailingSlash: true,
+    images: { unoptimized: true }
+};
 
 module.exports = nextConfig;
